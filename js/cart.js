9a67565940000235a3852416fc7e0ec63a30151f
Ananta Poudel <anantapoudel580@gmail.com>
2024-04-20 02:51:05 +0545

    Completed cart funcationlity

--- js/cart.js
+++ js/cart.js
@@ -5,10 +5,19 @@ DB.onInitialized(function () {
 function populateCart() {
   document.getElementById("loader").style.display = "flex";
   const cartList = document.getElementById("cartItems");
-  console.log(user.username);
+  const cartSummary = document.getElementById("cartSummary");
+
+  cartList.innerHTML = "";
   DB.getCartItemsByUsername(user.username, function (products) {
     if (products) {
+      if (products.length == 0) {
+        document.getElementById("loader").style.display = "none";
+        return;
+      }
+      let subTotal = 0;
+      let total = 0;
       products.forEach((item) => {
+        subTotal += item.product.price * item.quantity;
         const product = document.createElement("div");
         product.classList.add("product-item");
         product.innerHTML = `<img class="product-image" src="img/products/${
@@ -18,14 +27,18 @@ function populateCart() {
           <div class="product-info">
             <h1 class="product-name">${item.product.name}</h1>
             <p class="product-quantity">Quantity: ${item.quantity}</p>
-            <p class="product-total">Total: <b>${
-              item.quantity * item.product.price
-            }</b></p>
+            <p class="product-total">Price: ${item.product.price} | Total: ${
+          item.quantity * item.product.price
+        }</p>
           </div>
           <div class="product-controls">
-            <button class="p-btn icon outline rounded increase">+</button>
+            <button onclick="addToCart(${
+              item.product.id
+            })" class="p-btn icon outline rounded increase">+</button>
             <h1 class="quantity">${item.quantity}</h1>
-            <button class="p-btn icon outline error rounded decrease">
+            <button  onclick="removeFromCart(${
+              item.product.id
+            })" class="p-btn icon outline error rounded decrease">
               -
             </button>
           </div>
@@ -35,8 +48,76 @@ function populateCart() {
           document.getElementById("loader").style.display = "none";
         }, 2000);
       });
+
+      if (subTotal > 0) {
+        total = subTotal + subTotal * (0.13 + 0.1) + 50;
+        console.log(total);
+        document.getElementById("subTotalPrice").innerText = `CAD $${parseFloat(
+          subTotal
+        ).toFixed(2)}`;
+        document.getElementById("totalTax").innerText = `CAD $${parseFloat(
+          subTotal * 0.13
+        ).toFixed(2)}`;
+        document.getElementById("serviceCharge").innerText = `CAD $${parseFloat(
+          subTotal * 0.1
+        ).toFixed(2)}`;
+        document.getElementById("finalAmount").innerText = `CAD $${parseFloat(
+          total
+        ).toFixed(2)}`;
+        cartSummary.style.display = "flex";
+      }
     } else {
       console.log("Error loading products");
     }
   });
+}
+
+function addToCart(productId) {
+  DB.addToCart(
+    user.username,
+    productId,
+    function (success) {
+      var cartItemCount = document.getElementById("cartItemCount");
+      if (cartItemCount.innerText != "") {
+        let count = cartItemCount.innerText.substring(
+          1,
+          cartItemCount.innerText.length - 1
+        );
+        count++;
+        cartItemCount.innerText = `(${count})`;
+      } else {
+        cartItemCount.innerText = `(1)`;
+      }
+      showToast("s", success);
+      populateCart();
+    },
+    function (error) {
+      showToast("e", error);
+    }
+  );
+}
+
+function removeFromCart(productId) {
+  DB.removeFromCart(
+    user.username,
+    productId,
+    function (success) {
+      var cartItemCount = document.getElementById("cartItemCount");
+      if (cartItemCount.innerText != "") {
+        let count = cartItemCount.innerText.substring(
+          1,
+          cartItemCount.innerText.length - 1
+        );
+        count--;
+        cartItemCount.innerText = `(${count})`;
+      } else {
+        cartItemCount.innerText = ``;
+      }
+      showToast("s", success);
+      populateCart();
+    },
+    function (error) {
+      showToast("e", error);
+    }
+  );
 }
