5fd6d3707f6ed4d59e69ad5f83675a64d364a7f8
subash pariyar <pariyarsubash2055@gmail.com>
2024-04-19 22:09:01 -0400

    menu bar updated

--- js/index.js
+++ js/index.js
@@ -63,3 +63,13 @@ function showToast(type, message) {
     }, 2000);
   }
 }
+var MenuItems = document.getElementById("MenuItems");
+
+MenuItems.style.maxHeight = "0px";
+function menutoggle() {
+  if (MenuItems.style.maxHeight == "0px") {
+    MenuItems.style.maxHeight = "200px";
+  } else {
+    MenuItems.style.maxHeight = "0px";
+  }
+}
